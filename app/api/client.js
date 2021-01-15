fb09837e752b687e00bc0e58a0d8f26ff0847897
MarTCM <marwane.elbaraka@gmail.com>
2021-01-15 21:35:02 +0100

    hid client.js for security reasons

--- app/api/client.js
+++
@@ -1,8 +0,0 @@
-import { create } from "apisauce";
-
-const apiClient = create({
-  baseURL:
-    "https://revue-ecologique-tc1.herokuapp.com/BueXgzRdy68PnEZbHXRm3beF7Ls61W8A/api",
-});
-
-export default apiClient;
