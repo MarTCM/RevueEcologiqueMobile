d43f010655489e0b8f97c4d8fd3493082d443dd1
MarTCM <marwane.elbaraka@gmail.com>
2021-01-16 18:20:50 +0100

    added about and map sections in home screen

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { ImageBackground, StyleSheet, Text, View } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
+import MapView from "react-native-maps";
 import HomeSlideshow from "./HomeSlideshow";
 
 function HomeScreen(props) {
@@ -28,7 +29,54 @@ function HomeScreen(props) {
           </Text>
         </View>
       </View>
+      <Text
+        style={{
+          fontSize: 25,
+          paddingTop: 10,
+          paddingLeft: 10,
+          fontFamily: "serif",
+        }}
+      >
+        Articles Recents
+      </Text>
       <HomeSlideshow />
+      <View style={styles.paragraphContainer2}>
+        <Text
+          style={{
+            textAlign: "right",
+            fontSize: 25,
+            fontFamily: "sans-serif-bold",
+            paddingBottom: 5,
+          }}
+        >
+          À propos
+        </Text>
+        <View
+          style={{
+            borderBottomColor: "grey",
+            borderBottomWidth: 0.7,
+            width: 300,
+          }}
+        />
+        <Text style={styles.paragraph2}>
+          Ce site est une magazine écologique rédigée primairement par les
+          élèves du TCS1 du GS AlBayane. GS AlBayane est une école privée situé
+          à l'Av Tarik Ibn Ziad Hay Al Moustakbal Al Badil, comme le montre la
+          carte ci-dessous. Le créateur de ce site: Marwane ElBaraka, est lui
+          aussi un élève dans le GS AlBayane. Le professeur superviseur de cette
+          revue est: M. Hassan Ouchtaqalla.
+        </Text>
+      </View>
+      <MapView
+        style={styles.map}
+        provider={MapView.PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: 33.9038237389782,
+          longitude: -6.915270661380776,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
+      />
     </ScrollView>
   );
 }
@@ -72,6 +120,14 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontFamily: "sans-serif-light",
   },
+  paragraph2: {
+    textAlign: "center",
+    paddingTop: 15,
+    paddingLeft: 15,
+    paddingRight: 15,
+    fontSize: 17,
+    fontFamily: "sans-serif-light",
+  },
   paragraphContainer: {
     backgroundColor: "#d4d4d4",
     height: 175,
@@ -91,5 +147,29 @@ const styles = StyleSheet.create({
     elevation: 13,
 
     marginBottom: 45,
+  },
+  paragraphContainer2: {
+    backgroundColor: "#d4d4d4",
+    height: 300,
+    top: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 10,
+    margin: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.39,
+    shadowRadius: 8.3,
+
+    elevation: 13,
+
+    marginBottom: 30,
+  },
+  map: {
+    margin: 20,
+    height: 300,
   },
 });
